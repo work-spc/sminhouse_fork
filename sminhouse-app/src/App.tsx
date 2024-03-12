8a94459588f254ec6630845ec89cb3d8199976d3
Ilyas <qazwsxedc21012003@gmail.com>
2024-03-12 17:05:53 +0500

    готово

--- sminhouse-app/src/App.tsx
+++ sminhouse-app/src/App.tsx
@@ -1,9 +1,16 @@
 import { TopSection } from "./modules/top-section/top-section";
 import { HomePage } from "./pages/home-page";
-
+import background from "./icons/navbar/bg.png";
+import background_phone from "./icons/navbar/bg-phone.png";
 function App() {
   return (
     <>
+      <img className="absolute hidden md:block" src={background} alt="" />
+      <img
+        className="absolute w-full block md:hidden"
+        src={background_phone}
+        alt=""
+      />
       <HomePage></HomePage>
     </>
   );
--- sminhouse-app/src/modules/navbar/navbar.tsx
+++ sminhouse-app/src/modules/navbar/navbar.tsx
@@ -1,14 +1,16 @@
 import logo from "../../icons/navbar/logo.svg";
+import button from "../../icons/navbar/button.svg";
 import arrow_input from "../../icons/navbar/arrow-input.svg";
+
 export const Navbar: React.FC = () => {
   return (
-    <div className="max-w-1368 m-auto px-11 mb-10 py-5 flex justify-between items-center border rounded-2xl border-gray_border">
+    <div className=" max-w-1368 m-auto px-4 mdpx-11 mb-10 py-5 flex justify-between items-center border rounded-2xl border-gray_border">
       <button>
         <img src={logo} alt="" />
       </button>
 
       <div className="flex justify-center items-center">
-        <div className="flex">
+        <div className="hidden lg:flex">
           <button className="px-4">Решения</button>
           <button className="px-4">Проекты</button>
           <button className="px-4">Партнеры</button>
@@ -16,13 +18,16 @@ export const Navbar: React.FC = () => {
         </div>
       </div>
 
-      <div className="flex text-accent_font">
+      <div className="flex text-accent_font hidden lg:flex">
         <button className="mr-6">+7(800)800-80-80</button>
         <button className="flex px-6 py-3.5 items-center border rounded-2xl border-accent_border">
           <p className="mr-3">Войти</p>
           <img src={arrow_input} alt="" />
         </button>
       </div>
+      <button className="lg:hidden ">
+        <img src={button} alt="" />
+      </button>
     </div>
   );
 };
