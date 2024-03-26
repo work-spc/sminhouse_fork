b10c9fafbc8d943cc18b917c2643d15ee1122255
Ilyas <qazwsxedc21012003@gmail.com>
2024-03-26 13:56:27 +0500

    правки первого слайдера

--- sminhouse-app/src/components/button/button.tsx
+++ sminhouse-app/src/components/button/button.tsx
@@ -3,28 +3,21 @@ import "./style.css";
 
 interface ButtonProps {
   children: string;
-  // onClick?: MouseEventHandler<HTMLButtonElement>;
+
   type?: "outline";
 }
 
 export const Button: FC<ButtonProps> = ({ children, type }) => {
   if (type === "outline") {
     return (
-      <div
-        className="border-button-half  border-accent_border min-h-14 md:min-h-16 text-accent_font w-full md:w-auto p-0 md:py-4 md:px-10 rounded-lg text-lg md:text-xl hover:bg-accent_background hover:bg-opacity-10 active:bg-opacity-30 duration-300"
-        // style={{ borderWidth: "1.5px" }}
-        // onClick={onClick}
-      >
+      <button className="border-button-half  border-accent_border min-h-14 md:min-h-16 text-accent_font w-full md:w-auto p-0 md:py-4 md:px-10 rounded-lg text-lg md:text-xl hover:bg-accent_background hover:bg-opacity-10 active:bg-opacity-30 duration-300">
         {children}
-      </div>
+      </button>
     );
   }
   return (
-    <div
-      className="bg-accent_background min-h-14 md:min-h-16 text-white w-full  p-0 md:py-4 md:px-10 rounded-lg text-lg md:text-xl active:bg-button_click hover:bg-button_hover duration-300"
-      // onClick={onClick}
-    >
+    <button className="bg-accent_background min-h-14 md:min-h-16 text-white w-full  p-0 md:py-4 md:px-10 rounded-lg text-lg md:text-xl active:bg-button_click hover:bg-button_hover duration-300">
       {children}
-    </div>
+    </button>
   );
 };
--- sminhouse-app/src/components/swiper/swiper.tsx
+++ sminhouse-app/src/components/swiper/swiper.tsx
@@ -1,7 +1,9 @@
 import first from "../../icons/swiper-main/phone-first.png";
 import second from "../../icons/swiper-main/phone-second.png";
 import third from "../../icons/swiper-main/phone-third.png";
-import first_phone from "../../icons/swiper-main/first_phone.png";
+import first_phone from "../../icons/swiper-main/phone/1.png";
+import second_phone from "../../icons/swiper-main/phone/2.png";
+import third_phone from "../../icons/swiper-main/phone/3.png";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
@@ -28,18 +30,30 @@ export function SwiperMain() {
         <SwiperSlide className="md:py-12 flex flex-col SwiperSlide-1 items-center justify-center">
           <img className="hidden md:block px-16" src={first} alt="" />
           <div className="p-4 lock md:hidden flex-grow"></div>
-          <img className="block md:hidden" src={first_phone} alt="" />
+          <img
+            className="block md:hidden max-w-[226px]"
+            src={first_phone}
+            alt=""
+          />
         </SwiperSlide>
 
         <SwiperSlide className="md:py-12 flex flex-col SwiperSlide-2 items-center justify-center">
           <img className="hidden md:block px-16" src={second} alt="" />
           <div className="p-4 lock md:hidden flex-grow"></div>
-          <img className="block md:hidden" src={first_phone} alt="" />
+          <img
+            className="block md:hidden max-w-[226px]"
+            src={second_phone}
+            alt=""
+          />
         </SwiperSlide>
         <SwiperSlide className="md:py-12 flex flex-col SwiperSlide-3 items-center justify-center">
           <img className="hidden md:block px-16" src={third} alt="" />
           <div className="p-4 lock md:hidden flex-grow"></div>
-          <img className="block md:hidden" src={first_phone} alt="" />
+          <img
+            className="block md:hidden max-w-[226px]"
+            src={third_phone}
+            alt=""
+          />
         </SwiperSlide>
       </Swiper>
     </>
