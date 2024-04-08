be720d94e6f27bcf9dea47ea8f704bf6576ea849
Ilyas <qazwsxedc21012003@gmail.com>
2024-04-08 18:48:42 +0500

    поправил зум

--- sminhouse-app/src/App.tsx
+++ sminhouse-app/src/App.tsx
@@ -3,7 +3,7 @@ import background from "./icons/navbar/bg.svg";
 import "./App.css";
 function App() {
   return (
-    <div className="relative">
+    <div className="relative overflow-hidden">
       <img
         className="z-1 absolute right-[55%] top-[-11%] hidden md:block rotate"
         src={background}
